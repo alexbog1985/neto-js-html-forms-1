75c39a3c6f7f21d4a8b337f261bae462a666b112
Aleksey Bogdanov <aleksey@bogdanov77.ru>
2025-12-08 10:07:19 +0300

    начал писать тесты

---
+++ e2e/popover.test.js
@@ -0,0 +1,43 @@
+import puppeteer from "puppeteer";
+
+describe("Popover test", () => {
+  let browser;
+  let page;
+
+  beforeAll(async () => {
+    browser = await puppeteer.launch({
+      headless: false,
+      slowMo: 100,
+      devtools: true,
+    });
+
+    page = await browser.newPage();
+  });
+
+  test("should render popover on click", async () => {
+    await page.goto("http://localhost:9000");
+
+    await page.click("[data-toggle='popover']");
+    await page.waitForSelector(".popover", { visible: true });
+
+    const popover = await page.$(".popover");
+    const isVisible = popover.hidden !== false;
+    expect(isVisible).toBe(true);
+
+    const title = await page.$eval(".popover-header", (el) => {
+      return el.textContent.trim();
+    });
+    const content = await page.$eval(".popover-body", (el) => {
+      return el.textContent.trim();
+    });
+
+    expect(title).toBe("Popover title");
+    expect(content).toBe(
+      "And here's some amazing content. It's very engaging. Right?",
+    );
+  });
+
+  afterAll(async () => {
+    await browser.close();
+  });
+});
