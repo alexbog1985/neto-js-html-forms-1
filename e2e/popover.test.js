b626f5ab32681166e0a711f63146ed41b9152c54
Aleksey Bogdanov <aleksey@bogdanov77.ru>
2025-12-09 16:17:54 +0300

    добавил args: ["--no-sandbox"]

--- e2e/popover.test.js
+++ e2e/popover.test.js
@@ -6,9 +6,9 @@ describe("Popover test", () => {
 
   beforeAll(async () => {
     browser = await puppeteer.launch({
-      headless: false,
-      slowMo: 100,
-      devtools: true,
+      headless: "new",
+      // slowMo: 100,
+      // devtools: true,
       args: ["--no-sandbox"],
     });
 
